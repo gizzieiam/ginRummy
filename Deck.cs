4ef89d3f1c2b7fadb1d1ef65881c78f4ea424f45
codenameGizmo <shavonnecarson94@gmail.com>
2021-01-19 19:50:36 -0800

    start on building deck

--- Deck.cs
+++ Deck.cs
@@ -4,57 +4,18 @@ using UnityEngine;
 
 public class Deck : MonoBehaviour
 {
-    public int deckSize;
-    // Random rnd = new Random();
-    public Dictionary<string, int> currentDeck;
-    private string[] suit = {
-        "Hearts",
-        "Diamonds",
-        "Spades",
-        "Clovers",
-        "Joker",
-    };
-    
-
-    void Start()
+    public enum Suit
     {
-        deckSize = 54;
+        Clubs,
+        Diamonds,
+        Spades,
+        Hearts
     }
 
-    // Update is called once per frame
-    void Update()
+    public class Card
     {
-        shuffleDeck();
-    }
-    void shuffleDeck()
-    {
-        for(int i = 0; i< deckSize; i++)
-        {
-            int getSuit = Random.Range(1, 6);
-            int getNum = Random.Range(1,14);
-
-            if (getSuit == 1)
-            {
-
-            }
-            else if (getSuit == 2)
-            {
-
-            }
-            else if (getSuit == 3)
-            {
-
-            }
-            else if (getSuit == 4)
-            {
-
-            }
-            else if (getSuit == 5)
-            {
-                
-            }
-            currentDeck[getSuit] = getNum;
-            print(currentDeck);
-        }
+        public string DisplayName { get; set; }
+        public Suit Suit { get; set; }
+        public int Value { get; set; }
     }
 }
---
+++ Rules.md
@@ -0,0 +1,22 @@
+# Gin-rummy
+
+## Rules
+
+Random number of cards will be dealt out between the number of 1 to 13. Card
+amount wll always be an odd number. Players must draw one card either from
+the deck or the discard pile. Player can choose any card form the discard
+pile.
+
+If Player picks from the discard pile, he/she has to:
+
+    -Be able to score points with it or else you can not pick up.
+    - Take all cards on top of said card.
+
+You score points by matching at least 3 of the same number or at least 3
+numeric order cards of the same suit. You can score off of your opponents
+cards.
+
+To end your turn you put a card in the discard pile. If you do not have a
+card to put in the discard pile your turn ends. The round is only over once
+someones last card is put in the discrd pile. The first person to 200 or 500
+win.
